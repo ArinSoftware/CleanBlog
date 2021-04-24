2f7e0c3cbab41425adc00892664d7a7396a73a17
Arin <arinyazilim@gmail.com>
2021-04-24 15:34:52 +0300

    Clean Blog 2. Bölüm

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const ejs = require('ejs');
 
 const app = express();
 
+//Template Engine
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(express.static('public'));
+
+//Routes
 app.get("/", (req, res) => {
-  const blog = {
-    id: 1,
-    title: "Blog title",
-    description: "Blog description",
-  };
-  res.send(blog);
+  res.render('index');
+});
+
+app.get("/about", (req, res) => {
+  res.render('about');
+});
+
+app.get("/add_post", (req, res) => {
+  res.render('add_post');
 });
 
 const port = 3000;

38027d403f7ef38588cfd9674d131bf9595b6b83
Arin <arinyazilim@gmail.com>
2021-04-25 00:11:16 +0300

    Clean Blog Projesi 3. Bölüm

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
 const express = require("express");
-const ejs = require('ejs');
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const Post = require("./models/Post");
 
 const app = express();
+
+//connect DB
+mongoose.connect("mongodb://localhost/cleanblog-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Template Engine
 app.set("view engine", "ejs");
 
 //Middlewares
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Routes
-app.get("/", (req, res) => {
-  res.render('index');
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render("index", {
+    posts,
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.render('about');
+  res.render("about");
+});
+
+app.post("/posts", async (req, res) => {
+  await Post.create(req.body);
+  res.redirect("/");
 });
 
 app.get("/add_post", (req, res) => {
-  res.render('add_post');
+  res.render("add_post");
 });
 
 const port = 3000;
